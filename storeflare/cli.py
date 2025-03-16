2f0d6662e4bff94f784ef2d55793ba0744641ef7
k1m0ch1 <yahya.kimochi@gmail.com>
2025-03-16 12:50:45 +0700

    first init for cloudpeler stored metric

---
+++ storeflare/cli.py
@@ -0,0 +1,74 @@
+import os
+import sys
+
+import cfmetrics
+from datetime import datetime, timedelta
+from dotenv import load_dotenv
+from driver import jsondb
+
+load_dotenv()
+
+CF_API_KEY=os.getenv("CF_API_KEY")
+CF_EMAIL=os.getenv("CF_EMAIL")
+CF_ACCOUNTID=os.getenv("CF_ACCOUNTID")
+CF_ZONEID=os.getenv("CF_ZONEID")
+
+def checkAnalAndStore(dataAnal):
+   # store the data to database
+   for domain in getWebAnal['by_domain']['domain_lists']:
+       # check if domain already exist or not
+       domainData = jsondb.getDomain(domain)
+       dI = getWebAnal['by_domain']['domain_lists'].index(domain)
+       currentDomain = getWebAnal['by_domain']['domains'][dI]
+       if len(domainData) == 0:
+           # insert if not exists
+           saveData = jsondb.insertDomain(currentDomain) 
+       else:
+           # check if the data is that pulled is have a new data or not
+           # not yet tested
+           for domain in domainData:
+               for datedb in currentDomain['date_lists']:
+                   cdI = currentDomain['date_lists'].index(datedb)
+                   if datedb not in datedb['date_lists']:
+                        saveData = jsondb.insertDomainDate(currentDomain["name"], currentDomain['dates'][cdI])
+ 
+
+# need to check the database first
+# if the 30 days data is already stored or not
+# if already stored, check today data and stored
+# if not then stored for 30 days before
+cf = cfmetrics.Auth(CF_API_KEY, CF_EMAIL).Account(CF_ACCOUNTID).Zone(CF_ZONEID)
+mantap = cf.get_overview()
+
+import pdb; pdb.set_trace()
+
+oldest_date, newest_date = jsondb.getDateRange()
+if newest_date == datetime.now().strftime("%Y-%m-%d"):
+    print("Data is updated, end the program")
+    sys.exit(1)
+
+print("Proceed to check today data and update the data")
+
+daysDifference = (datetime.strptime(newest_date,"%Y-%m-%d")- datetime.strptime(oldest_date,"%Y-%m-%d")).days
+
+if daysDifference < 30:
+    getWebAnal = cf.get_web_analytics()
+
+    if len(getWebAnal['by_domain']['dates']) >0:
+        checkAnalAndStore(getWebAnal)
+    else:
+        print("No Data")
+
+# only get today
+else:
+    dateFormat = "%Y-%m-%dT%H:%M:%SZ"
+    startDate = f"{jsondb.getSecondNewestDate()}T23:59:59Z"
+    endDate = datetime.now().strftime(dateFormat)
+
+    print(f"Get data from {startDate} to {endDate}")
+    getWebAnal = cf.get_web_analytics(start_date=startDate, end_date=endDate)
+
+    checkAnalAndStore(getWebAnal)
+
+
+    
---
+++ storeflare/driver/jsondb.py
@@ -0,0 +1,110 @@
+import os
+
+from pydantic import BaseModel, Field
+from datetime import datetime
+from typing import List, Optional
+from tinydb import TinyDB, Query
+from dotenv import load_dotenv
+
+import os
+
+load_dotenv()
+
+CF_ACCOUNTID=os.getenv("CF_ACCOUNTID")
+CF_ZONEID=os.getenv("CF_ZONEID")
+DB_LOCATION=f"{os.getcwd()}/dumped_db"
+
+class Metrics(BaseModel):
+    page_viewwss: Optional[float] = None
+    visits: Optional[float]=None
+    requests: Optional[float]=None
+
+class DateEntry(BaseModel):
+    date: str
+    metrics: Metrics
+
+class DomainEntry(BaseModel):
+    name: str=Field(description="Domain Name")
+    dates: List[DateEntry]
+
+class DomainMetrics(BaseModel):
+    domains: List[DomainEntry]
+
+def createIfNotExists(location):
+    if not os.path.exists(location):
+        with open(location, "w") as f:
+            f.write("{}")
+
+
+WEB_ANAL_FILE = f"{DB_LOCATION}/{CF_ZONEID}_web_analytics.json"
+OVERVIEW_FILE = f"{DB_LOCATION}/{CF_ZONEID}_overview.json" 
+    
+createIfNotExists(WEB_ANAL_FILE)
+createIfNotExists(OVERVIEW_FILE)
+
+
+def getDomain(domain_name):
+    db = TinyDB(WEB_ANAL_FILE)
+    DomainQuery = Query()
+    result = db.search(DomainQuery.name == domain_name)
+
+    db.close()
+
+    return result
+
+def getDateDomain(domain_name, date):
+    db = TinyDB(WEB_ANAL_FILE)
+
+def insertDomain(domainEntry: DomainEntry):
+    db = TinyDB(WEB_ANAL_FILE)
+    inserted_id = db.insert(domainEntry)
+    getStatus = db.get(doc_id=inserted_id)
+
+    db.close()
+
+    return getStatus
+
+def insertDomainDate(domainName, dateEntry: DateEntry):
+    db = TinyDB(WEB_ANAL_FILE)
+    DomainQuery = Query()
+    result = db.search(DomainQuery.name == domainName)
+    if result:
+        domainData = result[0]
+        domainData["dates"].append(dateEntry)
+        db.update({"dates": domainData["dates"]}, DomainQuery.name == domainName)
+        return True
+    return False
+
+def getDateRange():
+    db = TinyDB(WEB_ANAL_FILE)
+    allDomain = db.all()
+    allDates = []
+
+    for domain in allDomain:
+        allDates.extend(domain["date_lists"])
+
+    if not allDates:
+        return [None, None]
+
+    dateObjects = [datetime.strptime(date, "%Y-%m-%d") for date in allDates]
+
+    db.close()
+
+    return [min(dateObjects).strftime("%Y-%m-%d"), max(dateObjects).strftime("%Y-%m-%d")]
+
+def getSecondNewestDate():
+    db = TinyDB(WEB_ANAL_FILE)
+    allDomains = db.all()
+    allDates = []
+    # Collect all dates
+    for domain in allDomains:
+        allDates.extend(domain["date_lists"]) 
+
+    if len(allDates) < 2:
+         return None
+    
+    date_objects = sorted([datetime.strptime(date, "%Y-%m-%d") for date in allDates], reverse=True)
+
+    second_newest_date = date_objects[1]  # Get the second newest date
+    return second_newest_date.strftime("%Y-%m-%d") 
+
